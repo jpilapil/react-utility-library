2c66aeb80aeb1c85361000d7b0a86b1a86b666f3
Justin Pilapil <57848030+jpilapil@users.noreply.github.com>
2020-06-04 22:32:57 -0700

    add uiData and functionUI

--- src/App.js
+++ src/App.js
@@ -1,56 +1,124 @@
-import React, { Component } from "react";
+import React from "react";
 import "./style/master.scss"; // applies global scss
 import { uiData } from "./data/ui"; // import uiData from data/ui
+import FunctionUI from "./components/FunctionUI";
 
-export default class App extends Component {
+export default class App extends React.Component {
    // everything that the webpage needs
    // happens before page is rendered
    // constructor method is a function that exists in the class we named App
    constructor() {
       super();
       console.log(uiData);
+      this.state = {
+         isFavoritesChecked: false,
+         allFuncs: uiData,
+         displayedFuncs: uiData,
+      };
+   }
+
+   toggleFavorites(e) {
+      this.setState({ isFavoritesChecked: !this.state.isFavoritesChecked });
+      const userInput = e.target.id; // checks id of the radio button user clicked, viewModeFavorites or viewModeAll
+      console.log(userInput);
+      const allFuncs = [...this.state.allFuncs];
+      if (userInput === "viewModeFavorites") {
+         const filteredFuncs = allFuncs.filter((func) => {
+            return func.isFavorite === true;
+         });
+         console.log(filteredFuncs);
+         this.setState({ displayedFuncs: filteredFuncs }); // display all filtered funcitons
+      } else {
+         this.setState({ displayedFuncs: allFuncs }); // display all functions
+      }
    }
 
    // render things in constructor
    // render method is a function that exists in the class we named App
    render() {
-      // console.log(uiData);
+      const getFunctionsNum = () => {
+         // gets the number of functions from uiData array
+         return uiData.length;
+      };
+
       return (
          <div className="container">
             <div className="row">
-               {uiData.map((component) => {
-                  return (
-                     <div className="col-12 col-lg-8 offset-lg-2 mb-5">
-                        <p className="name">
-                           {/* &nbsp; adds white space */}
-                           <b>{component.name}</b>&nbsp;-&nbsp;{component.desc}
-                        </p>
-                        <pre style={{ display: "none" }}>
-                           <code></code>
-                        </pre>
-                        <div className="actions float-right">
-                           <input
-                              type="text"
-                              className="form-control inline-action"
-                           />
-                           <input
-                              type="text"
-                              className="form-control inline-action"
-                           />
-                           <button className="btn btn-primary inline-action">
-                              Run
-                           </button>
-                        </div>
-                        <div className="clearfix mb-3"></div>
-                        <div
-                           className="alert alert-primary"
-                           style={{ display: "none" }}
-                        ></div>
-                        <div
-                           className="alert alert-danger"
-                           style={{ display: "none" }}
-                        ></div>
+               <div className="col-12 col-lg-8 offset-lg-2 mb-5">
+                  <h1 className="d-flex justify-content-center">
+                     JavaScript Function Library
+                  </h1>
+                  <p className="text-center lead mb-4">
+                     {getFunctionsNum()} functions documented
+                  </p>
+                  <div className="custom-control custom-radio custom-control-inline">
+                     <input
+                        type="radio"
+                        id="viewModeAll"
+                        name="viewMode"
+                        className="custom-control-input"
+                        checked={!this.state.isFavoritesChecked}
+                        // in the event that show all radio button is clicked, run toggleFavorites function
+                        onChange={(e) => {
+                           this.toggleFavorites(e);
+                        }}
+                     />
+                     <label
+                        className="custom-control-label"
+                        htmlFor="viewModeAll"
+                     >
+                        Show All
+                     </label>
+                  </div>
+                  <div className="custom-control custom-radio custom-control-inline">
+                     <input
+                        type="radio"
+                        id="viewModeFavorites"
+                        name="viewMode"
+                        className="custom-control-input"
+                        checked={this.state.isFavoritesChecked}
+                        onChange={(e) => {
+                           this.toggleFavorites(e);
+                        }}
+                     />
+                     <label
+                        className="custom-control-label"
+                        htmlFor="viewModeFavorites"
+                     >
+                        Favorites
+                     </label>
+                  </div>
+                  <div className="row mt-3">
+                     <div className="col-6">
+                        <input
+                           type="text"
+                           className="form-control"
+                           placeholder="Search all functions"
+                           aria-label="Search all functions"
+                           aria-describedby="searchInput"
+                           id="searchInput"
+                        />
                      </div>
+                     <div className="col-6">
+                        <select className=" form-control">
+                           <option>Most recent</option>
+                           <option>Oldest</option>
+                           <option>A - Z</option>
+                           <option>Z - A</option>
+                        </select>
+                     </div>
+                  </div>
+               </div>
+
+               {this.state.displayedFuncs.map((functionUI) => {
+                  const { name, desc, inputs } = functionUI;
+                  return (
+                     <FunctionUI
+                        key={name}
+                        name={name}
+                        desc={desc}
+                        inputs={inputs}
+                     />
                   );
                })}
                <h1 className="text-primary">carne</h1>
---
+++ src/components/FunctionUI.js
@@ -0,0 +1,42 @@
+import React from "react";
+export default function FunctionUI(props) {
+   // create function to render inputs
+   // loops through each number of inputs, push html input into empty input array
+   function renderInputs(num) {
+      const inputs = [];
+      for (let i = 0; i < num; i++) {
+         const id = `${props.name}, ${i}`;
+
+         inputs.push(
+            // push input elements into inputs array
+            <input
+               key={id}
+               id={id}
+               type="text"
+               className="form-control inline-action"
+            />
+         );
+      }
+      return inputs;
+   }
+   return (
+      <div className="col-12 col-lg-8 offset-lg-2 mb-5">
+         <p className="name">
+            {/* &nbsp; adds white space */}
+            <b>{props.name}</b>&nbsp;-&nbsp;{props.desc}
+         </p>
+         <pre style={{ display: "none" }}>
+            <code></code>
+         </pre>
+         <div className="actions float-right">
+            {/* input fields */}
+            {/* render array of elements */}
+            {renderInputs(props.inputs)}
+            <button className="btn btn-primary inline-action">Run</button>
+         </div>
+         <div className="clearfix mb-3"></div>
+         <div className="alert alert-primary" style={{ display: "none" }}></div>
+         <div className="alert alert-danger" style={{ display: "none" }}></div>
+      </div>
+   );
+}

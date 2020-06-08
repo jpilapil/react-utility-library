ec7d74d0455e5c3ecebb47a7684a6db4742d6ba2
Justin Pilapil <57848030+jpilapil@users.noreply.github.com>
2020-06-08 10:55:38 -0700

    add helper functions and implement

--- src/components/FunctionUI.js
+++ src/components/FunctionUI.js
@@ -1,48 +1,86 @@
 import React from "react";
 import utils from "../utils/utils";
+import { convertDataType } from "../utils/helpers";
 
-// stateless functional component, not rendered
-export default function FunctionUI(props) {
-   console.log(utils.add(4, 5));
-   // props = each individual function component from ui.js
-   // create function to render inputs
-   // loops through each number of inputs, push html input into empty input array
-   function renderInputs(num) {
-      const inputs = [];
-      for (let i = 0; i < num; i++) {
-         // creates an id which contains the prop name and the current number input
-         const id = `${props.name}-${i}`;
-         inputs.push(
-            // push input elements into inputs array
-            <input
-               key={id}
-               // shows the function name + input number as an id
-               id={id}
-               type="text"
-               className="form-control inline-action"
-            />
-         );
-      }
-      return inputs;
+export default class FunctionUI extends React.Component {
+   constructor() {
+      super();
+      this.state = {
+         isResultDisplayed: false,
+         isCodeDisplayed: false,
+         result: "",
+      };
    }
-   return (
-      <div className="col-12 col-lg-8 offset-lg-2 mb-5">
-         <p className="name">
-            {/* &nbsp; adds white space */}
-            <b>{props.name}</b>&nbsp;-&nbsp;{props.desc}
-         </p>
-         <pre style={{ display: "none" }}>
-            <code></code>
-         </pre>
-         <div className="actions float-right">
-            {/* input fields */}
-            {/* render array of elements */}
-            {renderInputs(props.inputs)}
-            <button className="btn btn-primary inline-action">Run</button>
+
+   render() {
+      const props = this.props;
+      console.log(utils.add(4, 5));
+      // props = each individual function component from ui.js
+      // create function to render inputs
+      // loops through each number of inputs, push html input into empty input array
+      function renderInputs(num) {
+         const inputs = [];
+         for (let i = 0; i < num; i++) {
+            // creates an id which contains the prop name and the current number input
+            const id = `input-${props.name}-${i}`;
+            inputs.push(
+               // push input elements into inputs array
+               <input
+                  key={id}
+                  // shows the function name + input number as an id
+                  id={id}
+                  type="text"
+                  className="form-control inline-action"
+               />
+            );
+         }
+         return inputs;
+      }
+
+      function getUserInput() {
+         console.log(props.name);
+         const inputValues = [];
+         for (let i = 0; i < props.inputs; i++) {
+            // loop through the input of each prop
+            const element = document.getElementById(`input-${props.name}-${i}`); // get the id of each input property name
+            const value = element.value;
+            const convertedValue = convertDataType(value);
+            inputValues.push(convertedValue);
+         }
+         console.log(inputValues);
+         const result = utils[name](...inputValues);
+      }
+
+      return (
+         <div className="col-12 col-lg-8 offset-lg-2 mb-5">
+            <p className="name">
+               {/* &nbsp; adds white space */}
+               <b>{props.name}</b>&nbsp;-&nbsp;{props.desc}
+            </p>
+            <pre style={{ display: "none" }}>
+               <code></code>
+            </pre>
+            <div className="actions float-right">
+               {/* input fields */}
+               {/* render array of elements */}
+               {renderInputs(props.inputs)}
+               <button
+                  className="btn btn-primary inline-action"
+                  onClick={() => getUserInput()}
+               >
+                  Run
+               </button>
+            </div>
+            <div className="clearfix mb-3"></div>
+            <div
+               className="alert alert-primary"
+               style={{ display: "none" }}
+            ></div>
+            <div
+               className="alert alert-danger"
+               style={{ display: "none" }}
+            ></div>
          </div>
-         <div className="clearfix mb-3"></div>
-         <div className="alert alert-primary" style={{ display: "none" }}></div>
-         <div className="alert alert-danger" style={{ display: "none" }}></div>
-      </div>
-   );
+      );
+   }
 }
---
+++ src/utils/helpers.js
@@ -0,0 +1,39 @@
+// application wide helper funcitons
+
+const safelyParseJSON = (str) => {
+   try {
+      JSON.parse(str);
+   } catch (error) {
+      return str;
+   }
+   return JSON.parse(str);
+};
+
+// test is user input is an object
+const isObject = (value) => {
+   if (
+      typeof value === "object" &&
+      value !== null &&
+      Array.isArray(value) === false
+   ) {
+      return true;
+   } else {
+      return false;
+   }
+   //    return typeof yourVariable === 'object' && yourVariable !== null
+};
+
+const convertDataType = (str) => {
+   const parsedJSON = safelyParseJSON(str);
+   if (str === "null") return null;
+   if (str === "undefined") return undefined;
+   if (str === "true") return true;
+   if (str === "false") return false;
+   // eslint-disable-next-line
+   if (str == Number(str)) return Number(str);
+   if (Array.isArray(parsedJSON)) return parsedJSON;
+   if (isObject(parsedJSON)) return parsedJSON;
+   return str;
+};
+
+export { isObject, convertDataType, safelyParseJSON };
